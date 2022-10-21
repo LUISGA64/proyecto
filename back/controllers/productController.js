867951606d9eb54b75bf5025adb56ab5b63bcf07
LUISGA64 <luisgalic64@gmail.com>
2022-10-21 17:28:33 -0500

    get prooductos

--- back/controllers/productController.js
+++ back/controllers/productController.js
@@ -2,14 +2,24 @@ const { json } = require("express")
 const { response } = require("../app")
 const producto = require("../models/products")
 
-exports.getProduct = (req, res, next) => {
+/*exports.getProduct = (req, res, next) => {
     res.status(200).json({
         success: true,
-        message: "Ruta de prueba funcionando"
+        message: "Ruta de prueba funcionando",
+    })
+} */
+
+exports.getProduct = async(req, res, next) => {
+    const products = await producto.find();
+    res.status(200).json({
+        success: true,
+        count: products.length,
+        products
     })
 }
 exports.newProduct = async(req, res, next) => {
     const product = await producto.create(req.body);
+
 
     res.status(201).json({
         success: true,
