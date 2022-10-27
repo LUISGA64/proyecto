bc40e0729069406463c9a8ee32ce38148c4bc63d
LUISGA64 <luisgalic64@gmail.com>
2022-10-27 18:59:35 -0500

    "cambios en proyec🇹🇬 "

--- back/app.js
+++ back/app.js
@@ -7,7 +7,8 @@ app.use(express.urlencoded({ extended: true }));
 
 //importar RUTAS
 const productos = require("./routes/product");
+const sales = require("./routes/sales");
 
-app.use('/api', productos)
-
+app.use('/api', productos);
+app.use('/api', sales);
 module.exports = app
