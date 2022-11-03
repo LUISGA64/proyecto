0c35dd81c024cb067e26edae79fe96da2e841729
LUISGA64 <luisgalic64@gmail.com>
2022-11-03 17:28:29 -0500

    rutas web

--- front/src/App.js
+++ front/src/App.js
@@ -3,13 +3,21 @@ import React from 'react';
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
 import Home from "./components/layout/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-    return (<div className="App" >
-        <Header/>
-        <Home />
-        <Footer/>
-    </div>
+    return (
+        <Router>
+            <div className="App" >
+                <Header />
+                <div className="container container-fluid">
+                    <Routes>
+                        <Route path="/Home" element={<Home />} />
+                    </Routes>
+                </div>
+                <Footer />
+            </div>
+        </Router>
     );
 }
 
